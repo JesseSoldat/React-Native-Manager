5a72a3722c0940b7c17bdacdcb0de00dd0e5255b
jesse <jsoldat@hotmail.com>
2017-04-23 09:43:16 +0700

    added most of the boiler plate

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { View, Text } from 'react-native';
+import { createStore } from 'redux';
+import reducers from './reducers';
+import firebase from 'firebase';
+
+class App extends Component {
+	componentWillMount() {
+		var config = {
+	    apiKey: "AIzaSyAw5uIZdlyPYjBuYoc8zRjbiv0-lxWANys",
+	    authDomain: "playground-3f11f.firebaseapp.com",
+	    databaseURL: "https://playground-3f11f.firebaseio.com",
+	    projectId: "playground-3f11f",
+	    storageBucket: "playground-3f11f.appspot.com",
+	    messagingSenderId: "1016399993027"
+	  };
+	  firebase.initializeApp(config);
+	}
+	render() {
+		return (
+			<Provider store={createStore(reducers)}>
+				<View>
+					<Text>Jlab</Text>
+				</View>
+			</Provider>
+		);
+	}
+}
+
+export default App;
